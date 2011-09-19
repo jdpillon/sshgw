f66d8df1f3b35e215f709708a7cb7c7b0cc6f72b
Jacques-Daniel PILLON <jdpillon@lesalternatives.org>
2011-09-19 17:07:37 +0200

    Options ok

--- lib/sshgw/arguments.rb
+++ lib/sshgw/arguments.rb
@@ -1,18 +1,37 @@
+require 'optparse'
+
 module Sshgw
   def self.parse(args)
     @options = {}
 
     optparse = OptionParser.new do |opts|
-      opts.banner = "Usage : sshgw -a username -t sshgw-host -f hidden-srv -a username_on_hiddensrv"
-      opts.on('-a', '--add', String, 'Add user') do |user|
+      opts.banner = "Usage : sshgw -n username -t sshgw-host -f hidden-srv -a username_on_hiddensrv"
+      opts.on('-n', '--new USER', 'Add user') do |user|
         @options[:user] = user
       end
+      opts.on('-t', '--to SSH-GATEWAY-HOST', 'ssh gateway') do |gwhost|
+        @options[:gwhost] = gwhost
+      end
+      opts.on('-f', '--for INTERNAL-HOST', 'Internal host') do |internalhost|
+        @options[:internalhost] = internalhost
+      end
+      opts.on('-a', '--as INTERNAL-USER', 'Internal user') do |internaluser|
+        @options[:internaluser] = internaluser
+      end
+
+
+
+
+
       opts.on('-h', '--help', 'Display this screen') do
+        puts opts
+        exit
+      end
+      if args.empty? or args.count < 4
         puts opts
         exit
       end
     end
     optparse.parse!(args)
-    p @options.inspect
   end
 end
