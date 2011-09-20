de92ba1c053c29d28472b14d478f891bbf71b04f
Jacques-Daniel PILLON <jdpillon@lesalternatives.org>
2011-09-20 15:32:01 +0200

    Version presque fonctionnelle

--- lib/sshgw.rb
+++ lib/sshgw.rb
@@ -8,14 +8,14 @@ require 'highline/import' # ask("...")
 
 module Sshgw
   class User
-    attr_accessor :name, :ssh_path, :authorized_key_path
+    attr_accessor :name, :ssh_path, :authorized_keys_path
     def initialize(name)
       @name = name
       # TODO paths to define
       @home_path = "/home/#{@name}"
       @ssh_path = "#{@home_path}/.ssh"
       @key_path = "#{@ssh_path}/id_rsa.pub"
-      @authorized_key_path = "#{@ssh_path}/authorized_key"
+      @authorized_keys_path = "#{@ssh_path}/authorized_keys"
     end
     def key
       # Return the user's key
@@ -26,9 +26,12 @@ module Sshgw
   end
 
   class LocalUser < User
-    def authorized_key
-      # Return the user's authorized_key file content
-      "THE CONTENT OF THE AUTHORIZED_KEY FILE"
+    def key
+      # Return the user's key file content
+      key = ""
+      File.open @key_path do |file|
+        key+=file.read
+      end
     end
 
   end
@@ -37,16 +40,23 @@ module Sshgw
     def ssh=ssh
       @ssh = ssh
     end
-    def authorized_key
-      # Return the user's authorized_key file content
+    def authorized_keys=(content)
+      # TODO : get random file name
+      file = File.open '/tmp/a','w'
+      file.write content
+      file.close
+      @ssh.scp.upload! '/tmp/a', @authorized_keys_path
+    end
+    def authorized_keys
+      # Return the user's authorized_keys file content
       c=""
       begin
-        c = @ssh.scp.download! @authorized_key_path
+        c = @ssh.scp.download! @authorized_keys_path
       rescue
-        puts "#{@authorized_key_path} does not exist!"
-        @ssh.exec!("touch #{@authorized_key_path}")
-        @ssh.exec!("chown #{@name}:#{@name} #{@authorized_key_path}")
-        c = @ssh.scp.download! @authorized_key_path
+        puts "#{@authorized_keys_path} does not exist!"
+        @ssh.exec!("touch #{@authorized_keys_path}")
+        @ssh.exec!("chown #{@name}:#{@name} #{@authorized_keys_path}")
+        c = @ssh.scp.download! @authorized_keys_path
       end  
     end
 
@@ -54,28 +64,7 @@ module Sshgw
 
 
   class Host
-    attr_reader :name, :local_user, :internal_host, :gw_user
-    def initialize(name_or_ip, local_user_name, internal_host = nil, internal_user_name = nil)
-      @name = name_or_ip
-      @local_user = LocalUser.new local_user_name
-      # ssh object to handle connections
-      @ssh = nil
-      if internal_host && internal_user_name
-        # We are the gw_host
-        @internal_host = Host.new internal_host, internal_user_name
-        @gw_user = RemoteUser.new @internal_host.name
-      end
-    end
-
-    def append_to_authorized_key
-      puts "Adding local user #{local_user.name}'s key to #{gw_user.name} authorized_key file."
-
-      content = "#{gw_user.authorized_key}\ncommand='ssh -t #{internal_host.local_user.name}@#{internal_host.name}' #{local_user.key}"
-      puts "New content :"
-      puts content
-      password = ask("Enter root password for #{internal_host.name}") { |q| q.echo = "*" }
-      @ssh = Net::SSH.start @internal_host.name, 'root', :password => password
-    end
+    attr_reader :name, :user
 
     def connect
       password = ask("Enter root password for #{name}") { |q| q.echo = "*" }
@@ -90,55 +79,104 @@ module Sshgw
 
     def close
       puts "Closing connection to #{@name}"
-      @ssh.close if @ssh
+      @gateway.shutdown!
+      #@ssh.close if @ssh
     end
 
-    def create_user
-      puts "Creating #{gw_user.name}"
-      @ssh.exec!("useradd -m #{gw_user.name}")
-      @ssh.exec!("mkdir #{gw_user.ssh_path}")
-      @ssh.exec!("chown #{gw_user.name}:#{gw_user.name} #{gw_user.ssh_path}")
-      @ssh.exec!("chmod 700 #{gw_user.ssh_path}")
-      gw_user.ssh = @ssh
-    end
   end
 
-  class RemoteHost < Host
+  # The local host as localhost
+  class LocalHost < Host
+    def initialize(name_or_ip, user_name)
+      @name = name_or_ip
+      @user = LocalUser.new user_name
+      # ssh object to handle connections
+    end
     def connect
-      password = ask("Enter root password for #{name}") { |q| q.echo = "*" }
+      puts "Connected to local host"
+    end
+    def exec(cmd)
+      system(cmd)
+    end
+  end
+  # The host used as ssh gateway
+  class GatewayHost < Host
+    attr_accessor :gateway, :remote_host
+    def initialize(name_or_ip, user_name)
+      @name = name_or_ip
+      @user = RemoteUser.new user_name
+    end
+
+    def connect
+      @password = ask("Enter root password for #{name}") { |q| q.echo = "*" }
       begin
-        @ssh = Net::SSH.start @name, 'root', :password => password
+        @gateway = Net::SSH::Gateway.new @name, 'root', :password => @password
         puts "Connected to #{@name} as root"
         rescue
           puts "Unable to connect to #{@name}"
           exit
       end
     end
+    
 
-    def hostname
+    def exec(cmd, host = 'localhost')
+      @gateway.ssh(host,'root',:password => @password) do |ssh|
+        puts ssh.exec!(cmd)
+      end
     end
+    def create_user
+      puts "Creating #{user.name}"
+      exec("useradd -m #{user.name}")
+      exec("mkdir #{user.ssh_path}")
+      exec("chown #{user.name}:#{user.name} #{user.ssh_path}")
+      exec("chmod 700 #{user.ssh_path}")
+      user.ssh = Net::SSH.start @name, 'root', :password => @password
+    end
+  end
+  # The host we want to reach through the ssh gateway
+  class RemoteHost < Host
+    def initialize(name_or_ip, user_name)
+      @name = name_or_ip
+      @user = RemoteUser.new user_name
+    end
+    def connect
+      @password = ask("Enter root password for #{name}") { |q| q.echo = "*" }
+    end
+  end
 
+  def self.append_user_key_to_authorized_keys_file(from, to)
+    puts "Adding local user #{from.user.name}'s key to #{to.user.name} authorized_keys file."
+
+    content = "#{to.user.authorized_keys}\ncommand='ssh -t #{to.remote_host.user.name}@#{to.remote_host.name}' #{from.user.key}\n"
+    to.user.authorized_keys=content
   end
 
   def self.run
+    options = Sshgw::parse(ARGV)
     # TODO : Add authors here
     puts "sshgw version : #{Sshgw::VERSION}"
-    options = Sshgw::parse(ARGV)
-    gw_host = Sshgw::Host.new options[:gwhost], options[:user], options[:internalhost], options[:internaluser]
-    puts "Create #{gw_host.gw_user.name} user on #{gw_host.name},"
-    puts "then add #{gw_host.local_user.name}'s public key to #{gw_host.gw_user.name}@#{gw_host.name}:#{gw_host.gw_user.authorized_key_path} with the command option :"
-    puts "command='ssh -t #{gw_host.internal_host.local_user.name}@#{gw_host.internal_host.name}' ssh-rsa..."
-    puts "When done, add #{gw_host.gw_user.name}'s public key to #{gw_host.internal_host.local_user.name}@#{gw_host.internal_host.name}:#{gw_host.internal_host.local_user.authorized_key_path}."
+
+    local_host                = LocalHost.new 'localhost', options[:user]
+    gateway_host              = GatewayHost.new options[:gwhost], options[:internalhost]
+    gateway_host.remote_host  = RemoteHost.new options[:internalhost], options[:internaluser] 
+
+    #gw_host = Sshgw::Host.new options[:gwhost], options[:user], options[:internalhost], options[:internaluser]
+    puts "Create #{gateway_host.user.name} user on #{gateway_host.name},"
+    puts "then add #{local_host.user.name}'s public key to #{gateway_host.user.name}@#{gateway_host.name}:#{gateway_host.user.authorized_keys_path} with the command option :"
+    puts "command='ssh -t #{gateway_host.remote_host.user.name}@#{gateway_host.remote_host.name}' ssh-rsa..."
+    puts "When done, add #{gateway_host.user.name}'s public key to #{gateway_host.remote_host.user.name}@#{gateway_host.remote_host.name}:#{gateway_host.remote_host.user.authorized_keys_path}."
     rep = ask("Are you ok with this ? (y/n)")
+    
+
     if rep == 'y' or rep =='Y'
-      gw_host.connect
-      gw_host.create_user
-      gw_host.append_to_authorized_key
+      gateway_host.connect
+      gateway_host.create_user
+      append_user_key_to_authorized_keys_file(local_host,gateway_host)
     else
       puts "Ok, bye!"
       exit
     end
-    gw_host.close
+    gateway_host.close
   end
 
 end
